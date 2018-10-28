88f3895b03719998a6862fed78ef502aa74b9cff
Davidemacchipoli <davidemacchi97@gmail.com>
2018-10-28 13:51:32 +0100

    upd

--- sketch.js
+++ sketch.js
@@ -1,44 +1,69 @@
-var myTable; //per caricare la tabella
+var myData;
+
+
+function preload() {
+    myData = loadJSON('assets/ds523.json');
+  }
+
 var balls = [];
 
 
-function preload(){
-  // put preload code here
-  myTable = loadTable('assets/Lottery_Mega_Millions_Winning_Numbers__Beginning_2002.csv', 'csv', 'header') // come scrivere i dati
-  //header significa che nel file c'è la prima riga che mi dice le categorie
-}
-
 function setup() {
-  createCanvas(windowWidth, windowHeight);
-  // put setup code here
-  var  tableObjects = myTable.getObject();
-  //var tableObject = myTable.getObject(); //crea un oggetto in Javascript
-  //console.log(tableObject);
+	createCanvas(windowWidth, windowHeight);
 
+  for(var i = 0; i < myData.daticonsumi.length; i++ ){
+    var astro = myData.daticonsumi[i];
 
-//ciclo in un oggetto
- for ( var rowIndex in tableObjects) { //questo loop prende ogni valore e crea un oggetto
-   var row = tableObjects[rowIndex];
-  // console.log(row.name); //mi da solo i nomi
-      //console.log(row.body);  //mi da solo la variabile body
-    //  console.log(row['body']); //modo diverso di scrivere le stesse cose
-      console.log(row['Mega Ball']); //devo ricordami di moltiplicare *1
+    var x = random(width);
+    var y = random(height);
+    var d = astro.Consumo + 50;
 
+    var newBall = new Ball(x, y, d);
+    balls.push(newBall);
 
-      for (var r = 0; r < row.length; r++) {
-          var from_long = rows[r].getNum("from_long")
-          var from_lat = rows[r].getNum("from_lat")
-          var x = map(from_long,-180,180,0,width)
-          var y = map(from_lat,-90,90,height,0)
-          ellipse(x,y,3,3)
+    console.log(astro);
+  }
 
-
-
-
- }
 
 }
 
 
 function draw() {
+	background(200);
+	for (var j = 0; j < balls.length; j++) {
+		balls[j].move();
+		balls[j].display();
+	}
+}
+
+function Ball(_x, _y, _diameter) {
+	// Properties defined by constructor
+	this.size = _diameter;
+	this.x = _x;
+	this.y = _y;
+	// Hardcoded properties
+	this.color = 'red';
+	this.speed = 2;
+
+	this.yDir = 1;
+	this.xDir = 1;
+	// Methods
+	this.move = function() {
+		this.x += this.speed * this.xDir;
+		this.y += this.speed * this.yDir;
+
+		if (this.y >= height || this.y <= 0) {
+			// if 1, set to -1, if -1, set to 1
+			this.yDir *= -1;
+		}
+
+		if (this.x >= width || this.x <= 0) {
+			this.xDir *= -1;
+		}
+	}
+
+	this.display = function() {
+		fill(this.color);
+		ellipse(this.x, this.y, this.size);
+	}
 }
